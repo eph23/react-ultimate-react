7a4022871424ef3feea0237ba5552f8fa9e532d4
ephraim <ephraims.818@gmail.com>
2025-10-11 18:13:32 +0600

    REACT-ULTIMATE-REACT--01-COMPONENTS-PROPS-JSX - 02. Creating and Reusing a Component

--- 01. Components, Props, and JSX/pizza-menu/src/App.js
+++ 01. Components, Props, and JSX/pizza-menu/src/App.js
@@ -1,7 +1,18 @@
+function Pizza() {
+    return (
+        <div>
+            <img src="pizzas/spinaci.jpg" alt="" />
+            <h1>Pizza Spinaci</h1>
+            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+        </div>
+    );
+}
+
 function App() {
     return (
         <div className="App">
             <h1>Hello React</h1>
+            <Pizza />
         </div>
     );
 }
