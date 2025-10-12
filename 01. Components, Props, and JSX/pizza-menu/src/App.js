7256bc102b0b1ff4eb46a902d4b60ea9cea4cb74
ephraim <ephraims.818@gmail.com>
2025-10-13 03:51:54 +0600

    REACT-ULTIMATE-REACT--01-COMPONENTS-PROPS-JSX - 07. Rendering Lists

--- 01. Components, Props, and JSX/pizza-menu/src/App.js
+++ 01. Components, Props, and JSX/pizza-menu/src/App.js
@@ -1,3 +1,48 @@
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
+
 // ANCHOR Header Component
 function Header() {
     return (
@@ -12,18 +57,12 @@ function Menu() {
     return (
         <main className="menu">
             <h2>Our Menu</h2>
-            <Pizza
-                name="Pizza Spinaci"
-                ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-                photoName="pizzas/spinaci.jpg"
-                price={10}
-            />
-            <Pizza
-                name="Pizza Funghi"
-                ingredients="Tomato, mushrooms, spinach, and ricotta cheese"
-                price={12}
-                photoName="pizzas/funghi.jpg"
-            />
+
+            <ul className="pizzas">
+                {pizzaData.map((pizza) => (
+                    <Pizza pizzaObj={pizza} key={pizza.name} />
+                ))}
+            </ul>
         </main>
     );
 }
@@ -31,14 +70,14 @@ function Menu() {
 // ANCHOR Pizza Component
 function Pizza(props) {
     return (
-        <div className="pizza">
-            <img src={props.photoName} alt="" />
+        <li className="pizza">
+            <img src={props.pizzaObj.photoName} alt="" />
             <div>
-                <h3>{props.name}</h3>
-                <p>{props.ingredients}</p>
-                <span>{props.price + 2}</span>
+                <h3>{props.pizzaObj.name}</h3>
+                <p>{props.pizzaObj.ingredients}</p>
+                <span>{props.pizzaObj.price + 2}</span>
             </div>
-        </div>
+        </li>
     );
 }
 
