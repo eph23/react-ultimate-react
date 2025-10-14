0537bce88ab182c543b4ad1a14f01711771c6d26
ephraim <ephraims.818@gmail.com>
2025-10-15 04:41:50 +0600

    REACT-ULTIMATE-REACT--01-COMPONENTS-PROPS-JSX - 13. React Fragments

--- 01. Components, Props, and JSX/pizza-menu/src/App.js
+++ 01. Components, Props, and JSX/pizza-menu/src/App.js
@@ -61,12 +61,20 @@ function Menu() {
     return (
         <main className="menu">
             <h2>Our Menu</h2>
+
             {numPizzas > 0 ? (
-                <ul className="pizzas">
-                    {pizzaData.map((pizza) => (
-                        <Pizza pizzaObj={pizza} key={pizza.name} />
-                    ))}
-                </ul>
+                <>
+                    <p>
+                        Authentic Italian cuisine. 6 creative dishes to choose
+                        from. All from our stone oven, all organic, all
+                        delicious.
+                    </p>
+                    <ul className="pizzas">
+                        {pizzaData.map((pizza) => (
+                            <Pizza pizzaObj={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </>
             ) : (
                 <p>We are still working on our menu. Please comeback later</p>
             )}
