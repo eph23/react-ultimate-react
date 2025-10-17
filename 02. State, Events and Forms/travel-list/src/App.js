25932adef1802d6e909a9f27de2c0dc202c30082
ephraim <ephraims.818@gmail.com>
2025-10-18 05:21:34 +0600

    REACT-ULTIMATE-REACT--02-STATE-EVENTS-FORMS - PROJECT--FAR-AWAY -  01. Building the Layout

---
+++ 02. State, Events and Forms/travel-list/src/App.js
@@ -0,0 +1,37 @@
+function Logo() {
+    return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+    return (
+        <div className="add-form">
+            <h3>What do you need for your trip?</h3>
+        </div>
+    );
+}
+
+function PackingList() {
+    return <div className="list"></div>;
+}
+function Status() {
+    return (
+        <footer className="stats">
+            <em>
+                You have X items on your list, and you already packed X (X%)
+            </em>
+        </footer>
+    );
+}
+
+function App() {
+    return (
+        <div className="App">
+            <Logo />
+            <Form />
+            <PackingList />
+            <Status />
+        </div>
+    );
+}
+
+export default App;
