9cf27da27e06832e3180a1e116234b7082ecd5f4
ephraim <ephraims.818@gmail.com>
2025-10-19 02:42:19 +0600

    REACT-ULTIMATE-REACT--02-STATE-EVENTS-FORMS - PROJECT--FAR-AWAY -  02. Rendering the Items List

--- 02. State, Events and Forms/travel-list/src/App.js
+++ 02. State, Events and Forms/travel-list/src/App.js
@@ -1,3 +1,10 @@
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+    { id: 3, description: "Charger", quantity: 1, packed: true },
+    { id: 3, description: "Charger", quantity: 1, packed: true },
+];
+
 function Logo() {
     return <h1>🏝️ Far Away 🧳</h1>;
 }
@@ -11,8 +18,28 @@ function Form() {
 }
 
 function PackingList() {
-    return <div className="list"></div>;
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item} />
+                ))}
+            </ul>
+        </div>
+    );
 }
+
+function Item({ item }) {
+    return (
+        <li>
+            <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+                {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>
+    );
+}
+
 function Status() {
     return (
         <footer className="stats">
