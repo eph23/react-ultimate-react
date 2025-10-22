828ae0e3129b58bb191ce893c57c5046368edc14
ephraim <ephraims.818@gmail.com>
2025-10-23 00:51:05 +0600

    REACT-ULTIMATE-REACT--03.-THINKING-IN-REACT - PROJECT--FAR-AWAY -  04. Calculating Statistics as Derived State

--- 03. Thinking in React/far-away/src/App.jsx
+++ 03. Thinking in React/far-away/src/App.jsx
@@ -27,7 +27,6 @@ function Form({ onAddItems }) {
             id: Date.now(),
         };
 
-        console.log(newItem);
         onAddItems(newItem);
 
         setDescription("");
@@ -91,11 +90,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     );
 }
 
-function Status() {
+function Status({ items }) {
+    if (!items.length) {
+        return (
+            <p className="stats">
+                <em>Start adding some items to your packing list 🚀</em>
+            </p>
+        );
+    }
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = Math.round((numPacked / numItems) * 100);
+
     return (
         <footer className="stats">
             <em>
-                You have X items on your list, and you already packed X (X%)
+                {percentage === 100
+                    ? "You got everything packed. Ready to go! ✈️"
+                    : `You have ${numItems} items on your list, and you already packed
+                ${numPacked} (${percentage}%)`}
             </em>
         </footer>
     );
@@ -129,7 +142,7 @@ function App() {
                 onDeleteItem={handleDeleteItem}
                 onToggleItem={handleToggleItem}
             />
-            <Status />
+            <Status items={items} />
         </div>
     );
 }
