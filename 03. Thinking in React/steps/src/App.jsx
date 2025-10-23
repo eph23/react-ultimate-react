dd1fa95c26e0943cbeb5dcc49c03edcf8c522ba5
ephraim <ephraims.818@gmail.com>
2025-10-24 05:42:29 +0600

    REACT-ULTIMATE-REACT--03.-THINKING-IN-REACT - PROJECT--STEPS - 08. The Children Prop

---
+++ 03. Thinking in React/steps/src/App.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+
+const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+];
+
+function Button({ textColor, bgColor, onClick, children }) {
+    return (
+        <button
+            style={{
+                backgroundColor: bgColor,
+                color: textColor,
+            }}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
+}
+
+function App() {
+    let [step, setStep] = useState(1);
+    const [isOpen, setIsOpen] = useState(true);
+
+    function handlePrevious() {
+        if (step > 1) {
+            setStep((currentStep) => currentStep - 1);
+        }
+    }
+    function handleNext() {
+        if (step < 3) {
+            setStep((currentStep) => currentStep + 1);
+        }
+    }
+
+    return (
+        <>
+            <button
+                className="close"
+                onClick={() => setIsOpen((isOpen) => !isOpen)}
+            >
+                &times;
+            </button>
+            {isOpen && (
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={step >= 1 ? "active" : ""}>1</div>
+                        <div className={step >= 2 ? "active" : ""}>2</div>
+                        <div className={step >= 3 ? "active" : ""}>3</div>
+                    </div>
+                    <p className="message">
+                        Step {step}: {messages[step - 1]}
+                    </p>
+                    <div className="buttons">
+                        <Button
+                            bgColor="#7950f2"
+                            textColor="#fff"
+                            onClick={handlePrevious}
+                        >
+                            <span>⏮️</span>Previous
+                        </Button>
+                        <Button
+                            bgColor="#7950f2"
+                            textColor="#fff"
+                            onClick={handleNext}
+                        >
+                            Next<span>⏭️</span>
+                        </Button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
+
+export default App;
