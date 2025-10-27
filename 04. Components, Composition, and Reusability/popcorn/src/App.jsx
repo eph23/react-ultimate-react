d64d2f98ab4648e43c243f3d6ee07d33f17ce447
ephraim <ephraims.818@gmail.com>
2025-10-27 13:22:03 +0600

    REACT-ULTIMATE-REACT--04-components-composition-and-reusability - PROJECT--POPCORN  - 04. Using Composition to Make a Reusable Box

--- 04. Components, Composition, and Reusability/popcorn/src/App.jsx
+++ 04. Components, Composition, and Reusability/popcorn/src/App.jsx
@@ -103,21 +103,22 @@ function MovieList({ movies }) {
     );
 }
 
-function ListBox({ children }) {
-    const [isOpen1, setIsOpen1] = useState(true);
+function Box({ children }) {
+    const [isOpen, setIsOpen] = useState(true);
 
     return (
         <div className="box">
             <button
                 className="btn-toggle"
-                onClick={() => setIsOpen1((open) => !open)}
+                onClick={() => setIsOpen((open) => !open)}
             >
-                {isOpen1 ? "–" : "+"}
+                {isOpen ? "–" : "+"}
             </button>
-            {isOpen1 && children }
+            {isOpen && children}
         </div>
     );
 }
+
 
 function WatchedSummery({ watched }) {
     const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
@@ -182,34 +183,13 @@ function WatchedMovieList({ watched }) {
     );
 }
 
-function WatchedBox() {
-    const [watched, setWatched] = useState(tempWatchedData);
-    const [isOpen2, setIsOpen2] = useState(true);
-
-    return (
-        <div className="box">
-            <button
-                className="btn-toggle"
-                onClick={() => setIsOpen2((open) => !open)}
-            >
-                {isOpen2 ? "–" : "+"}
-            </button>
-            {isOpen2 && (
-                <>
-                    <WatchedSummery watched={watched} />
-                    <WatchedMovieList watched={watched} />
-                </>
-            )}
-        </div>
-    );
-}
-
 function Main({ children }) {
     return <main className="main">{children}</main>;
 }
 
 export default function App() {
     const [movies, setMovies] = useState(tempMovieData);
+    const [watched, setWatched] = useState(tempWatchedData);
 
     return (
         <>
@@ -219,10 +199,13 @@ export default function App() {
                 <NumResults movies={movies} />
             </NavBar>
             <Main>
-                <ListBox>
+                <Box>
                     <MovieList movies={movies} />
-                </ListBox>
-                <WatchedBox />
+                </Box>
+                <Box>
+                    <WatchedSummery watched={watched} />
+                    <WatchedMovieList watched={watched} />
+                </Box>
             </Main>
         </>
     );
