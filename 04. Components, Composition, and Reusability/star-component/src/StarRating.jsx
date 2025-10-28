062d4bcfe236d1b17ee73f1e9017f7c215376788
ephraim <ephraims.818@gmail.com>
2025-10-28 08:01:44 +0600

    REACT-ULTIMATE-REACT--04-components-composition-and-reusability - STAR-RATING - 03. Handling Hover Events

--- 04. Components, Composition, and Reusability/star-component/src/StarRating.jsx
+++ 04. Components, Composition, and Reusability/star-component/src/StarRating.jsx
@@ -23,7 +23,8 @@ const starStyle = {
 };
 
 const StarRating = ({ maxRating = 5 }) => {
-    const [rating, setRating] = useState(1);
+    const [rating, setRating] = useState(0);
+    const [tempRating, setTempRating] = useState(0);
 
     function handleRating(rating) {
         setRating(rating);
@@ -36,20 +37,32 @@ const StarRating = ({ maxRating = 5 }) => {
                     <Star
                         key={index}
                         onRate={() => handleRating(index + 1)}
-                        full={rating >= index + 1}
+                        full={
+                            tempRating
+                                ? tempRating >= index + 1
+                                : rating >= index + 1
+                        }
+                        onHoverIn={() => setTempRating(index + 1)}
+                        onHoverOut={() => setTempRating(0)}
                     />
                 ))}
             </div>
-            <p style={textStyle}>{rating || ""}</p>
+            <p style={textStyle}>{tempRating || rating || ""}</p>
         </div>
     );
 };
 
 export default StarRating;
 
-const Star = ({ onRate, full }) => {
+const Star = ({ onRate, full, onHoverIn, onHoverOut }) => {
     return (
-        <span role="button" style={starStyle} onClick={onRate}>
+        <span
+            role="button"
+            style={starStyle}
+            onClick={onRate}
+            onMouseEnter={onHoverIn}
+            onMouseLeave={onHoverOut}
+        >
             {full ? (
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
