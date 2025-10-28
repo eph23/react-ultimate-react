ae0bc63f5e91324659f01bd5142f745a56b40fb0
ephraim <ephraims.818@gmail.com>
2025-10-28 07:50:35 +0600

    REACT-ULTIMATE-REACT--04-components-composition-and-reusability - STAR-RATING - 02. Creating the Stars

--- 04. Components, Composition, and Reusability/star-component/src/index.jsx
+++ 04. Components, Composition, and Reusability/star-component/src/index.jsx
@@ -6,7 +6,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <StarRating maxRating={5} />
-        <StarRating maxRating={10} />
-        <StarRating />
     </React.StrictMode>
 );
