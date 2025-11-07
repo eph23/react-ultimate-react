b53a34ec9d67e5b462991e56bd4260472668d6b1
ephraim <ephraims.818@gmail.com>
2025-11-07 06:05:38 +0600

    REACT-ULTIMATE-REACT--06-effects-and-data-fetching - 11. Cleaning Up Data Fetching

--- 06. Effects and Data Fetching/popcorn/src/App.jsx
+++ 06. Effects and Data Fetching/popcorn/src/App.jsx
@@ -364,12 +364,15 @@ export default function App() {
 
     useEffect(
         function () {
+            const controller = new AbortController();
+
             async function fetchMovies() {
                 try {
                     setIsLoading(true);
                     setError("");
                     const res = await fetch(
-                        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+                        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+                        { signal: controller.signal }
                     );
 
                     if (!res.ok) throw new Error(`Something went wrong...`);
@@ -379,8 +382,11 @@ export default function App() {
                         throw new Error(`Movie not found`);
 
                     setMovies(data.Search);
+                    setError("");
                 } catch (err) {
-                    setError(err.message);
+                    if (err.name !== "AbortError") {
+                        setError(err.message);
+                    }
                 } finally {
                     setIsLoading(false);
                 }
@@ -393,6 +399,10 @@ export default function App() {
             }
 
             fetchMovies();
+
+            return function () {
+                controller.abort();
+            };
         },
         [query]
     );
