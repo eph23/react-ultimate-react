08a9f2664789503366d9de2ab4318573c5cd51ca
ephraim <ephraims.818@gmail.com>
2025-11-16 02:32:17 +0600

    REACT-ULTIMATE-REACT--09. REACT-ROUTER - PROJECT--WORLD-WISE - 01. Implementing Main Pages and Routes

--- 09. React Router/worldwise/src/App.jsx
+++ 09. React Router/worldwise/src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
     return (
-        <div>
-            <h1>WorldWise</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Homepage />}></Route>
+                <Route path="product" element={<Product />}></Route>
+                <Route path="pricing" element={<Pricing />}></Route>
+                <Route path="*" element={<PageNotFound />}></Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
