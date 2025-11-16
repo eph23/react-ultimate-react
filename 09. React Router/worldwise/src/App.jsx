d7efcb810df630f6c326ba6ffd104f8b0ec312fa
ephraim <ephraims.818@gmail.com>
2025-11-16 22:44:58 +0600

    REACT-ULTIMATE-REACT--09. REACT-ROUTER - PROJECT--WORLD-WISE - 03. Using CSS Modules

--- 09. React Router/worldwise/src/App.jsx
+++ 09. React Router/worldwise/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
                     <Route path="/" element={<Homepage />}></Route>
                     <Route path="product" element={<Product />}></Route>
                     <Route path="pricing" element={<Pricing />}></Route>
+                    <Route path="app" element={<AppLayout />}></Route>
                     <Route path="*" element={<PageNotFound />}></Route>
                 </Routes>
             </BrowserRouter>
