1e6b239ab4126f869b0ac9503e750926ca83bb0b
ephraim <ephraims.818@gmail.com>
2025-11-19 19:01:27 +0600

    REACT-ULTIMATE-REACT--09. REACT-ROUTER - PROJECT--WORLD-WISE - 12. Programmatic Navigation With Navigate

--- 09. React Router/worldwise/src/App.jsx
+++ 09. React Router/worldwise/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -43,12 +43,7 @@ function App() {
                     <Route path="app" element={<AppLayout />}>
                         <Route
                             index
-                            element={
-                                <CityList
-                                    cities={cities}
-                                    isLoading={isLoading}
-                                />
-                            }
+                            element={<Navigate replace to="cities" />}
                         />
                         <Route
                             path="cities"
