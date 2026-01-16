f8a9189fabea5aa8fd9b176f4b84eb9dd4710e2a
ephraim <ephraims.818@gmail.com>
2026-01-16 20:22:28 +0600

    REACT-URC - STATE-EVENTS-FORMS - 03. Creating a State Variable with useState

--- 01. React Fundamentals/02. State, Events, and Forms/steps-counter/src/App.jsx
+++ 01. React Fundamentals/02. State, Events, and Forms/steps-counter/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
     "Learn React ⚛️",
     "Apply for jobs 💼",
@@ -5,21 +7,29 @@ const messages = [
 ];
 
 function App() {
-    const step = 1;
+    const [step, setStep] = useState(1);
 
     function handlePrevious() {
-        alert("PREVIOUS");
+        if (step === 1) {
+            return;
+        } else {
+            setStep(step - 1);
+        }
     }
     function handleNext() {
-        alert("NEXT");
+        if (step >= messages.length) {
+            return;
+        } else {
+            setStep(step + 1);
+        }
     }
 
     return (
         <div className="steps">
             <div className="numbers">
-                <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-                <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-                <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+                <div className={step >= 1 ? "active" : ""}>1</div>
+                <div className={step >= 2 ? "active" : ""}>2</div>
+                <div className={step >= 3 ? "active" : ""}>3</div>
             </div>
             <p className="message">
                 Step {step}: {messages[step - 1]}
