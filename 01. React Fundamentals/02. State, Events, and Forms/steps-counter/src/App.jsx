a3dfe9335b1415431af89d945fa7ced3c623f480
ephraim <ephraims.818@gmail.com>
2026-01-16 19:59:33 +0600

    REACT-URC - STATE-EVENTS-FORMS - 02. Handling Events

--- 01. React Fundamentals/02. State, Events, and Forms/steps-counter/src/App.jsx
+++ 01. React Fundamentals/02. State, Events, and Forms/steps-counter/src/App.jsx
@@ -7,6 +7,13 @@ const messages = [
 function App() {
     const step = 1;
 
+    function handlePrevious() {
+        alert("PREVIOUS");
+    }
+    function handleNext() {
+        alert("NEXT");
+    }
+
     return (
         <div className="steps">
             <div className="numbers">
@@ -18,10 +25,16 @@ function App() {
                 Step {step}: {messages[step - 1]}
             </p>
             <data className="buttons">
-                <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+                <button
+                    style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                    onClick={handlePrevious}
+                >
                     Previous
                 </button>
-                <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+                <button
+                    style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                    onClick={handleNext}
+                >
                     Next
                 </button>
             </data>
