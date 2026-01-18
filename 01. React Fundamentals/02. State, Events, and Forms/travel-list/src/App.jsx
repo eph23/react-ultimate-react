c0e495889c6bda83f04d6284af6fa6d89262fbf3
ephraim <ephraims.818@gmail.com>
2026-01-19 04:37:22 +0600

    REACT-URC - STATE-EVENTS-FORMS - PRACTICE-PROJECT - Far Away - 04. Controlled Elements

--- 01. React Fundamentals/02. State, Events, and Forms/travel-list/src/App.jsx
+++ 01. React Fundamentals/02. State, Events, and Forms/travel-list/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
     { id: 1, description: "Passports", quantity: 2, packed: false },
     { id: 2, description: "Socks", quantity: 12, packed: true },
@@ -9,22 +11,50 @@ function Logo() {
 }
 
 function Form() {
+    const [description, setDescription] = useState("");
+    const [quantity, setQuantity] = useState(1);
+
     function handleSubmit(event) {
         event.preventDefault();
+
+        if (!description) return;
+
+        const newItem = {
+            id: Date.now(),
+            description,
+            quantity,
+            packed: false,
+        };
+
+        console.log(newItem);
+
+        setDescription("");
+        setQuantity(1);
     }
 
     return (
         <form className="add-form" onSubmit={handleSubmit}>
-            <h3>What do you need for your trip?</h3>
-            <select>
+            {" "}
+            <h3>What do you need for your trip?</h3>{" "}
+            <select
+                value={quantity}
+                onChange={(event) => setQuantity(Number(event.target.value))}
+            >
+                {" "}
                 {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
                     <option value={num} key={num}>
-                        {num}
+                        {" "}
+                        {num}{" "}
                     </option>
-                ))}
-            </select>
-            <input type="text" placeholder="Item..." />
-            <button>Add</button>
+                ))}{" "}
+            </select>{" "}
+            <input
+                type="text"
+                placeholder="Item..."
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+            />{" "}
+            <button>Add</button>{" "}
         </form>
     );
 }
