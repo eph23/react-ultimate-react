eb0bb601e93da7c470b47f8d236d9f433f6ee464
ephraim <ephraims.818@gmail.com>
2026-01-23 02:04:56 +0600

    REACT-URC - THINKING-IN-REACT - 07. Moving Components Into Seperate Files

---
+++ 01. React Fundamentals/03. Thinking In React/travel-list-v2/src/States.jsx
@@ -0,0 +1,23 @@
+export function States({ items }) {
+    if (!items.length)
+        return (
+            <p className="stats">
+                <em>Start adding some items to your packing list 🚀</em>{" "}
+            </p>
+        );
+
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = (numPacked / numItems) * 100;
+
+    return (
+        <footer className="stats">
+            <em>
+                {percentage === 100
+                    ? `You got everything packed 🛬`
+                    : `You have ${numItems} items in your list and you already packed
+                ${numPacked}(${percentage}%)`}
+            </em>
+        </footer>
+    );
+}
