718da93309d35875afa7f936e1e72d944f81c7e9
ephraim <ephraims.818@gmail.com>
2026-01-23 01:24:15 +0600

    REACT-URC - THINKING-IN-REACT - 06. Claring the List

--- 01. React Fundamentals/03. Thinking In React/travel-list-v2/src/App.jsx
+++ 01. React Fundamentals/03. Thinking In React/travel-list-v2/src/App.jsx
@@ -61,7 +61,7 @@ function Form({ onAddItems }) {
     );
 }
 
-function PackingList({ items, onDeleteItem, onToggleItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
     const [sortBy, setSortBy] = useState("input");
     let sortedItems;
 
@@ -98,6 +98,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
                     <option value="description">Sort by description</option>
                     <option value="packed">Sort by packed status</option>
                 </select>
+                <button onClick={onClearList}>Clear list</button>
             </div>
         </div>
     );
@@ -161,6 +162,11 @@ function App() {
         );
     }
 
+    function handleClearList() {
+        const confirmed = window.confirm("Are you sure?");
+        if (confirmed) setItems([]);
+    }
+
     return (
         <div className="app">
             <Logo />
@@ -169,6 +175,7 @@ function App() {
                 items={items}
                 onDeleteItem={handleDeleteItem}
                 onToggleItem={handleToggleItem}
+                onClearList={handleClearList}
             />
             <States items={items} />
         </div>
