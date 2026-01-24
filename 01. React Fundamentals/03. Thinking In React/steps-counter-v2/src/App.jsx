b5ea2accefb11bae08383f7ca4142cc858afc17e
ephraim <ephraims.818@gmail.com>
2026-01-25 04:23:48 +0600

    REACT-URC - THINKING-IN-REACT - 09. More Reusability With the children Prop

--- 01. React Fundamentals/03. Thinking In React/steps-counter-v2/src/App.jsx
+++ 01. React Fundamentals/03. Thinking In React/steps-counter-v2/src/App.jsx
@@ -41,9 +41,8 @@ function App() {
                         <div className={step >= 2 ? "active" : ""}>2</div>
                         <div className={step >= 3 ? "active" : ""}>3</div>
                     </div>
-                    <p className="message">
-                        Step {step}: {messages[step - 1]}
-                    </p>
+
+                    <StepMessage step={step}>{messages[step - 1]}</StepMessage>
                     <data className="buttons">
                         <Button
                             bgColor="#7950f2"
@@ -63,6 +62,14 @@ function App() {
                 </div>
             )}
         </>
+    );
+}
+
+function StepMessage({ step, children }) {
+    return (
+        <div className="message">
+            <h3>Step {step}</h3> {children}
+        </div>
     );
 }
 
