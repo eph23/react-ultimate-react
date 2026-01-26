143ac716af4c7e9b7bf68c728e991b5c2f274a33
ephraim <ephraims.818@gmail.com>
2026-01-26 21:49:37 +0600

    REACT-URC - THINKING-IN-REACT - SECTION-PROJECT - 02. Building the Static App Forms

--- 01. React Fundamentals/04. SECTION PROJECT - EAT-N-SPLIT/eat-n-split/src/App.jsx
+++ 01. React Fundamentals/04. SECTION PROJECT - EAT-N-SPLIT/eat-n-split/src/App.jsx
@@ -31,9 +31,52 @@ function Friend({ friend }) {
                     {friend.name} owe's you {Math.abs(friend.balance)}€
                 </p>
             )}
-            <button className="button">Select</button>
+            <Button>Select</Button>
         </li>
     );
+}
+
+function FormAddFriend() {
+    return (
+        <form className="form-add-friend">
+            <label>📝Friend's Name</label>
+            <input type="text" />
+
+            <label>🖼️ Image URL</label>
+            <input type="text" />
+
+            <Button>Add</Button>
+        </form>
+    );
+}
+
+function FormSplitBill() {
+    return (
+        <form className="form-split-bill">
+            <h2>Split a Bill with X</h2>
+
+            <label>💰Bill Value</label>
+            <input type="text" />
+
+            <label>💸Your Expense</label>
+            <input type="text" />
+
+            <label>🤑 X's expense</label>
+            <input type="text" disabled />
+
+            <label>💳 Who is paying the bill</label>
+            <select>
+                <option value="">You</option>
+                <option value="">X</option>
+            </select>
+
+            <Button>Split Bill</Button>
+        </form>
+    );
+}
+
+function Button({ children }) {
+    return <button className="button">{children}</button>;
 }
 
 function App() {
@@ -41,7 +84,9 @@ function App() {
         <div className="app">
             <div className="sidebar">
                 <FriendsList />
+                <FormAddFriend />
             </div>
+            <FormSplitBill />
         </div>
     );
 }
